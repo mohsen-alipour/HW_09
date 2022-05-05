0330781f8df29da0e47c03020a9a139cd008b68d
mohsen-test <alipourmohsen2015@gmail.com>
2022-05-05 11:19:42 +0430

    Add project files.

---
+++ HW_09/Controllers/MemberController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HW_09.Models;
+namespace HW_09.Controllers
+{
+    public class MemberController : Controller
+    {
+        private MemberRepository O_product_R;
+        public MemberController()
+        {
+            O_product_R = new MemberRepository();
+        }
+        public IActionResult Index()
+        {
+            var List_M = O_product_R.GetAllMember();
+            return View(List_M);
+            
+        }
+        [HttpGet]
+        public IActionResult AddMember()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddMember(Member item)
+        {
+            O_product_R.Addmember(item);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            var x = O_product_R.GetMemberdatail(id);
+            O_product_R.DelteMember(x);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditMember(int id)
+        {
+            var x = O_product_R.GetMemberdatail(id);
+            return View(x);
+        }
+        [HttpPost]
+        public IActionResult EditMember(Member item)
+        {
+
+            O_product_R.EditMember(item);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DetailMember(int id)
+        {
+            var x = O_product_R.GetMemberdatail(id);
+            return View(x);
+
+        }
+        [HttpGet]
+        public IActionResult search()
+        {          
+            return View();
+        }
+        [HttpPost]
+        public IActionResult search(Member item)
+        {
+            var x = O_product_R.search(item.Name,item.Family);
+
+            return View("resultsearch", x);
+        }
+
+
+    }
+}
---
+++ HW_09/Models/Member.cs
@@ -0,0 +1,36 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using HW_09.Models.Enum;
+namespace HW_09.Models
+{
+    public class Member
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "نام")]
+        public string Name { get; set; }
+
+        [Display(Name = "نام خانوادگی")]
+        public string Family { get; set; }
+
+        [Display(Name = "تاریخ ثبت")]
+        public DateOnly Date_sabt { get; set; }
+
+        [Display(Name = "تاریخ تولد")]
+        public DateOnly Date_birthday { get; set; }
+
+        [Display(Name = "کد ملی")]
+        public int Codemeli { get; set; }
+
+        [Display(Name = "نوع عضویت")]
+        public MembershipEnum Typemember { get; set; }
+
+        [Display(Name = "شماره تماس")]
+        public string Tel { get; set; }
+
+        [Display(Name = "جنسیت")]
+        public GenderEnum Gender { get; set; }
+
+
+    }
+}
